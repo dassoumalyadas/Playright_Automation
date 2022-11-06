921cb7de56b42a9169e2021b5d0930190c7f5fba
dassoumalyadas <dassoumalyadas@gmail.com>
2022-11-06 13:10:51 +0530

    after first successful test run

--- new-project/tests/UIBasicsTest.spec.js
+++ new-project/tests/UIBasicsTest.spec.js
@@ -3,13 +3,12 @@ const {test}=require('@playwright/test');
 test("Browser context Playright test",async ({browser})=>{
     //Playright code.
     const context=await browser.newContext();
-    const page=await context.newPage();
-    await page.goto("www.google.com");
+    const page=await(await context).newPage();
+    await page.goto("http://google.com");
 
 });
 
 test("Page Playright test",async ({page})=>{
     //Playright code without browser context.
-    await page.goto("www.google.com");
-
+    await page.goto("http://bing.com");
 });
